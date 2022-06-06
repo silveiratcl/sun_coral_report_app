73b6a5982ed8e151de398da0073a944b0866b769
Thiago Silveira <62719017+silveiratcl@users.noreply.github.com>
2022-06-06 15:22:29 -0300

    user auth removed for data visualization

--- app/markers/views.py
+++ app/markers/views.py
@@ -14,29 +14,30 @@ from markers.serializers import MarkerSerializer
 class MarkerViewSet(viewsets.ModelViewSet):
     """Marker view set."""
 
-    bbox_filter_field = "location"
+    bbox_filter_field = 'location'
     filter_backends = (filters.InBBoxFilter,)
     queryset = Marker.objects.all()
     serializer_class = MarkerSerializer
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    # just for visualiza if the user is authenticated
+    #permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        """Retrieve markers for authenticated user"""
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+#    def get_queryset(self):
+ #       """Retrieve markers for authenticated user"""
+  #      return self.queryset.filter(user=self.request.user).order_by('-id')
 
-    def get_serializer_class(self):
-        """Return the serializer class for request"""
-        if self.action == 'list':
-            return serializer.MarkerSerializer
+   # def get_serializer_class(self):
+    #    """Return the serializer class for request"""
+     #   if self.action == 'list':
+      #      return serializer.MarkerSerializer #########
 
-        return self.serializer_class
+       # return self.serializer_class
 
-    def perform_create(self, serializer):
-        """Create a new marker"""
-        serializer.save(user=self.request.user)
+#    def perform_create(self, serializer):
+ #       """Create a new marker"""
+   #     serializer.save(user=self.request.user)
 
 class MarkersMapView(TemplateView):
     """Markers map view."""
 
-    template_name = "map.html"
+    template_name = 'map.html'
